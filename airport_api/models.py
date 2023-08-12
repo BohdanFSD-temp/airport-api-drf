605b17dcf52099350d81f094904b64a48fb1fe89
BohdanFSD-temp <luitko009@gmail.com>
2023-08-12 11:42:35 +0000

    Created models for airport_api

--- airport_api/models.py
+++ airport_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="source"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="destination"
+    )
+    distance = models.IntegerField()
