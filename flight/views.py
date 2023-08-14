8017e6ec5cfe9e7fbd39164d90e1648c45a076a8
BohdanFSD-temp <luitko009@gmail.com>
2023-08-14 14:36:57 +0000

    Created views for flight app

--- flight/views.py
+++ flight/views.py
@@ -1,1 +1,25 @@
+from rest_framework import viewsets
 
+
+from flight.models import AirplaneType, Airplane, Crew, Flight
+from flight.serializers import AirplaneTypeSerializer, AirplaneSerializer, CrewSerializer, FlightSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
